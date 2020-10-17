1211716c7d1cf4513eaf0c8cdebb3e873fa76434
priyankapandey0021 <73017785+priyankapandey0021@users.noreply.github.com>
2020-10-17 14:41:54 +0530

    Create frequencyOfListItems.py

---
+++ frequencyOfListItems.py
@@ -0,0 +1,20 @@
+# Function to count frequency of each element  
+import collections 
+  
+# it returns a dictionary data structure whose  
+# keys are array elements and values are their  
+# corresponding frequencies {1: 4, 2: 4, 3: 2,  
+# 5: 2, 4: 1} 
+def CountFrequency(arr): 
+    return collections.Counter(arr)   
+  
+# Driver function 
+if __name__ == "__main__": 
+  
+    arr = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5] 
+    freq = CountFrequency(arr) 
+  
+    # iterate dictionary named as freq to print 
+    # count of each element 
+    for (key, value) in freq.items(): 
+        print (key, " -> ", value) 
